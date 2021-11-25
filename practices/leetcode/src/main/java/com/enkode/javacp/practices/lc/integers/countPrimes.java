eda93e4aa9489d4682751118f11d37e39c26c4b8
sangeethapradeep <sangeethapradeep94@gmail.com>
2021-11-25 11:53:18 -0600

    Problem 204

---
+++ practices/leetcode/src/main/java/com/enkode/javacp/practices/lc/integers/countPrimes.java
@@ -0,0 +1,34 @@
+package com.enkode.javacp.practices.lc.integers;
+
+/**
+ * Problem 204
+ *
+ * <pre>
+ *       Test Cases:
+ *              10
+ *              1
+ *              0
+ * </pre>
+ */
+public class countPrimes {
+
+  public static void main(String[] args) {
+    int n = 10;
+    System.out.println(countPrime(n));
+  }
+
+  private static int countPrime(int n) {
+    if (n < 3) return 0;
+    int counter = 1;
+    boolean[] notPrime = new boolean[n];
+    for (int i = 3; i < n; i += 2) {
+      if (!notPrime[i]) {
+        counter++;
+        for (int j = i * 3; j < n; j += i) {
+          notPrime[j] = true;
+        }
+      }
+    }
+    return counter;
+  }
+}
